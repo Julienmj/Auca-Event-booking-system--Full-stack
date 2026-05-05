0ec07859bcd48a4caa734d7b744e5c77f7d827f3
Julienmj <julienmj@auca.ac.rw>
2026-05-05 13:30:40 +0200

    Add EventRepository - JpaRepository for Event

---
+++ src/main/java/eventbooking/repository/EventRepository.java
@@ -0,0 +1,7 @@
+package eventbooking.repository;
+
+import eventbooking.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
