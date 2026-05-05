62bde3faee12044e0ba1aaec4025c0219a6f842f
Julienmj <julienmj@auca.ac.rw>
2026-05-05 13:30:44 +0200

    Add RegistrationRepository - JpaRepository for Registration

---
+++ src/main/java/eventbooking/repository/RegistrationRepository.java
@@ -0,0 +1,7 @@
+package eventbooking.repository;
+
+import eventbooking.model.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+}
